ec8502d67a245906d4c6266a4c95a0d99becd86e
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-03-12 15:13:27 +0800

    Fix setup for heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 const noteRoutes = require('./routes/note')
 
-app.use('/note', noteRoutes)
+app.use(noteRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on port:${port}`)
